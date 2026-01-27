ea24ea2c1b9b319e36a48f0277cfe9573f6bf34b
Manh <manhselu@gmail.com>
2026-01-27 08:31:46 +0100

    Model User repair

--- source/model/Users.js
+++ source/model/Users.js
@@ -30,7 +30,7 @@ const userSchema = new mongoose.Schema(
         type: String,
         default:
           "https://static.vecteezy.com/system/resources/previews/036/280/651/original/default-avatar-profile-icon-social-media-user-image-gray-avatar-icon-blank-profile-silhouette-illustration-vector.jpg",
-      },
+      },  
       publicId: {
         type: String,
         default: null,
